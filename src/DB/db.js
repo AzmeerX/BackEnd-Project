2d461a599c4a06d62d3a77c00ad5caf595eff36e
Ghost <azmeerfarhan786@gmail.com>
2025-06-17 20:38:01 +0500

    API's and middleware

--- src/DB/db.js
+++ src/DB/db.js
@@ -1,8 +1,12 @@
+import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+dotenv.config();
+
 const connectDB = async () => {
     try {
+        console.log("🔗 Connecting to:", `${process.env.MONGODB_URL}/${DB_NAME}`);
         const connectionObj = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(`DB Connected.\nDB HOST: ${connectionObj.connection.host}`);
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import connectDB from "./DB/db.js";
 dotenv.config({
     path: './env'
 });
+
+connectDB()
+.then(() => {
+    application.listen(process.env.PORT)
+})
+.catch((err) => {
+    console.log("MongoDB connection failed");
+});
