df233ac15662d112cf89d49f4005e7cfe8579141
Ghost <azmeerfarhan786@gmail.com>
2025-06-19 20:00:30 +0500

    Fixed some errors and multer.js

--- src/DB/db.js
+++ src/DB/db.js
@@ -5,14 +5,14 @@ import { DB_NAME } from "../constants.js";
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        console.log("🔗 Connecting to:", `${process.env.MONGODB_URL}/${DB_NAME}`);
-        const connectionObj = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log(`DB Connected.\nDB HOST: ${connectionObj.connection.host}`);
-    } catch (error) {
-        console.log("ERROR: ", error);
-        process.exit(1);
-    }
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log(`DB Connected.\nDB HOST: ${connectionInstance.connection.host}`);
+    return connectionInstance; 
+  } catch (error) {
+    console.error("Mongo connection error:", error);
+    throw error;
+  }
 };
 
 export default connectDB;
