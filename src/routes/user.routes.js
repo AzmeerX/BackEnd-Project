34792e797e35f8ea44048cdc4a51198572d2aec4
Ghost <azmeerfarhan786@gmail.com>
2025-07-07 20:25:39 +0500

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logOutUser, refreshAccessToken, changePassword } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logOutUser, refreshAccessToken, changePassword, getUser, updateProfile, updateAvatar, updateCoverImage, getChannel, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.js";
 
@@ -29,5 +29,23 @@ router.route("/refresh-token").post(refreshAccessToken);
 
 
 router.route("/change-password").post(verifyJWT, changePassword);
+
+
+router.route("/current-user").get(verifyJWT, getUser);
+
+
+router.route("/update-account").patch(verifyJWT, updateProfile);
+
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+
+router.route("/c/:username").get(verifyJWT, getChannel);
+
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
